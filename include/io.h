9b741df63dc3739c3a393d80110645372eaa3648
Kenneth Topolovec <ktopolovec@yahoo.com>
2021-07-05 15:04:41 -0400

    Moved CMAKE file to top of repo and added files

---
+++ include/io.h
@@ -0,0 +1,3 @@
+// Function prototypes
+
+int add(int a, int b);
--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -1,15 +1,16 @@
 // This is a sample Hello World program to be built
 //
 #include <iostream>
+#include <io.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << "You have entered " << argc
-         << " arguments:" << std::endl;
-  
-    for (int i = 0; i < argc; ++i)
-        std::cout << argv[i] << std::endl;
-  
-    return 0;
-}
+	std::cout << "You have entered " << argc << " arguments:" << std::endl;
+	for (int i = 0; i < argc; ++i)
+	{
+        	std::cout << argv[i] << std::endl;
+	}
 
+	std::cout << "Addition is " << add(1, 2) << std::endl; 
+    	return 0;
+}
